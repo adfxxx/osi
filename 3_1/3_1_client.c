32614db7e8d3f2d4db90531549ec211c723d5350
adfxxx <vdoyanova@mail.ru>
2023-11-24 18:01:29 +0300

    up

---
+++ 3_1/3_1_client.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define MAX_TEXT 100
+#define MAX_PRIOR 5
+
+typedef struct{
+    long mtype;
+    char mtext[MAX_TEXT];
+}msg_buf;
+
+int main(int argc, char *argv[]){
+    key_t server_key = ftok("server_key", 'r');
+    int server_msgid = msgget(server_key, 0);
+    if(server_msgid < 0){
+        perror("Server does not exist.\n");
+        exit(1);
+    }
+
+    key_t client_key = ftok("client_key", 'r');
+    int client_msgid = msgget(client_key, 0);
+    if(client_msgid < 0){
+        perror("Client does not exist.\n");
+        exit(1);
+    }
+    
+    char *line = NULL;
+    size_t len = 0;
+    char str[MAX_TEXT];
+    int count = 0;
+    int prior = 0;
+    for(int i = 1; i < argc; i++){
+        int j = 0;
+        int k = 0;
+        int flag = 0;
+        FILE *file = fopen(argv[i], "r");
+        if(!file){
+            perror("File is not open.\n");
+            exit(1);
+        }
+        while(getline(&line, &len, file) != -1){
+            msg_buf msg;
+            sscanf(line, "prior=%d", &prior);
+            msg.mtype = prior;
+            while(flag != 2){
+                if(line[j] == '='){
+                    flag++;
+                }
+                j++;
+            }
+            while(k < MAX_TEXT && line[j] != '\"'){
+                str[k] = line[j];
+                k++;
+                j++;
+            }
+            strcpy(msg.mtext, str);
+            msgsnd(server_msgid, &msg, sizeof(msg), 0); //отправка в очередь
+            printf("Message is sent.\n");
+            count++;
+        }
+        fclose(file);
+    }
+
+    msg_buf msg_2;
+    while(count){
+        client_msgid = msgget(client_key, 0);
+        if(client_msgid < 0){
+            break;
+        }
+        if(msgrcv(client_msgid, &msg_2, sizeof(msg_2), -MAX_PRIOR, IPC_NOWAIT) < 0){
+            continue;
+        }
+        printf("Processed message %s with priority %ld.\n", msg_2.mtext, msg_2.mtype);
+        count--;
+    }
+
+    printf("Client is over.\n");
+    return 0;
+}
---
+++ 3_1/3_1_server.c
@@ -0,0 +1,72 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_TEXT 100
+#define MAX_PRIOR 5
+
+typedef struct{
+    long mtype;
+    char mtext[MAX_TEXT];
+}msg_buf;
+
+int create_queue(key_t key);
+void process_message(msg_buf msg, int msgid);
+
+int main(){
+    key_t server_key = ftok("server_key", 'r');
+    int server_msgid = create_queue(server_key);
+
+    key_t client_key = ftok("client_key", 'r');
+    int client_msgid = create_queue(client_key);
+
+    printf("Server is running.\n");
+    while(1){
+        msg_buf msg;
+        if(msgrcv(server_msgid, &msg, sizeof(msg), 0, IPC_NOWAIT) < 0){ //возвращает колво байт. тип 0 - любое сбщ принято
+            continue;
+        }
+        printf("Server received message.\n");
+        if(msg.mtype == 1){ //проверка типа на завершение программы
+            printf("Received exit message. Server closing.\n");
+            msgsnd(client_msgid, &msg, sizeof(msg), IPC_NOWAIT);
+            break;
+        }
+        process_message(msg, client_msgid);
+    }
+
+    msgctl(server_msgid, IPC_RMID, 0);
+    printf("Server queue is destroyed.\n");
+    msgctl(client_msgid, IPC_RMID, 0);
+    printf("Client queue is destroyed.\n");
+
+    printf("Server is over.\n");
+    return 0;
+}
+
+int create_queue(key_t key){
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+    return msgid;
+}
+
+void process_message(msg_buf msg, int msgid){
+    int length = strlen(msg.mtext);
+    char temp[MAX_TEXT];
+    strcpy(temp, msg.mtext);
+    char prev;
+    char cur;
+    for(int i = 0; i < length; i++){
+        prev = cur;
+        cur = temp[i];
+        if(prev == cur && (cur == ' ' || cur == '\t' || cur == '\n' || cur == '!' || cur == '.' || cur == ',' || cur == '?')){
+            for(int j = i; j < length; j++){
+                temp[j] = temp[j+1];
+            }
+        }
+    }
+    msg.mtype = 2;
+    strcpy(msg.mtext, temp);
+    msgsnd(msgid, &msg, sizeof(msg), IPC_NOWAIT);//отправка в очередь
+}
